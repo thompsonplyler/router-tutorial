9a861841213257b8cd3625c462715238c6b63ad4
Thompson Plyler <nealok.g@gmail.com>
2022-06-13 13:56:47 -0400

    Finished tutorial.

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,41 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import ReactDOM from "react-dom/client";
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+import App from "./App";
+import Expenses from "./routes/expenses";
+import Invoices from "./routes/invoices";
+import Invoice from "./routes/invoice";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+const root = ReactDOM.createRoot(
+  document.getElementById("root")
+);
+root.render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} >
+        <Route path="expenses" element={<Expenses />} />
+        <Route path="invoices" element={<Invoices />} >
+          <Route 
+            index 
+            element={
+              <main style={{padding: '1rem'}}>
+                <p>Select an invoice.</p>
+              </main>
+            }/>
+            <Route path=":invoiceId" element={<Invoice />}/>
+        </Route>
+        <Route
+          path="*"
+          element={
+            <main style={{ padding: "1rem" }}>
+              <p>There's nothing here!</p>
+            </main>
+      }
+    />
+      </Route>
+    </Routes>
+  </BrowserRouter>
+);
